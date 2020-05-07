92f4391a7a9f35ca8298851a3f5d2f650dc4d7c8
nemusou <ihachihiro@team-lab.com>
2020-05-07 18:53:53 +0900

    first commit

---
+++ components/Button.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  button.button(@click="handleClick") {{ numStr }}
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+
+@Component
+export default class Button extends Vue {
+  @Prop({ type: String, required: true})
+  public numStr!: String;
+
+  public handleClick() {
+    console.log(this.$props.numStr)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  width: 80px;
+  height: 80px;
+  border-radius: 50px;
+  font-size: 2.2rem;
+  color: #f3f3f3;
+  background: linear-gradient(145deg, #4da7db, #5bc6ff);
+  box-shadow:  20px 20px 60px #479bcc, 
+               -20px -20px 60px #63d7ff;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  outline: none;
+  appearance: none;
+  border: none;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  .container
+    .gridhead hellooooooooooooooo
+    .div(v-for="key in keys")
+      Button(:numStr="key")
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+import Button from '~/components/Button.vue';
+// import Logo from '~/components/Logo.vue'
+
+@Component({
+  components: {
+    Button
+    // Logo
+  }
+})
+export default class Index extends Vue {
+  keys = [
+    'C', '÷', '×', 'del',
+    '7', '8', '9', '-',
+    '4', '5', '6', '+',
+    '1', '2', '3', '=',
+    '%', '0', '.',
+  ];
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  background: #55b9f3;
+  display: grid;
+  grid-template-rows: 1fr 15% 15% 15% 15% 15%;
+  grid-template-columns: 25% 25% 25% 25%;
+}
+
+.gridhead {
+  grid-column: span 4;
+}
+</style>
